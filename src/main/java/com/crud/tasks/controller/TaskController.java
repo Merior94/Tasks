3fb8adef227e7252e9804bda1ad7a792a24abdca
merior94 <merior94@gmail.com>
2022-06-26 16:58:10 +0200

    19.3 Implementacja funkcjonalności - response entity

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,7 +5,9 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -34,10 +36,13 @@ public class TaskController {
 //    }
 
     @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
-        return taskMapper.mapToTaskDto(
-                service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
-        );
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        try{
+            return new ResponseEntity<>(taskMapper.mapToTaskDto(service.getTask(taskId)), HttpStatus.OK);
+        }
+        catch (TaskNotFoundException e) {
+            return new ResponseEntity<>(new TaskDto(0L, "There is no task with id:" + taskId, ""), HttpStatus.BAD_REQUEST);
+        }
     }
 
 
