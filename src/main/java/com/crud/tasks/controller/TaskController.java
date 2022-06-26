4dae279ab23ef6b35ccdf99724e739e48c5ada5a
merior94 <merior94@gmail.com>
2022-06-26 13:52:04 +0200

    18.4 Tworzenie Controllera

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{id}")
+    public TaskDto getTask(@PathVariable("id") Long taskId) {
+        switch (taskId.intValue()) {
+            case 1:
+                return new TaskDto(1L, "test title1", "test_content1");
+            case 2:
+                return new TaskDto(2L, "test title2", "test_content2");
+            default:
+                return new TaskDto(0L, "test title", "test_content");
+        }
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @PostMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @PutMapping
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
